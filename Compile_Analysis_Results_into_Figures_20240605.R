78952ae956c175a15c86133525d0ba2262a238c1
Alexander E. Lipka <ael54@cornell.edu>
2024-06-14 17:05:22 -0500

    Worked on an R script that compiles the results into figures

--- Compile_Analysis_Results_into_Figures_20240605.R
+++ Compile_Analysis_Results_into_Figures_20240605.R
@@ -11,26 +11,131 @@ library('MASS')
 library(gplots)
 library(sommer)
 
+#######################################################
+#######################################################
+#######################################################
+#Experimental code - delete once a pipeline has been developed
+# maybe consider putting this into a function
+factor.A <- 1
+factor.B <- 0.05
+factor.C <- 0.05
+factor.D <- 0.05
+this.rep <- 2
+
+this.setting <-  paste(factor.A,".FactorA..",factor.B, ".FactorB..",
+                       factor.C,".FactorC..",factor.D,".FactorD..",
+                       this.rep, ".Rep", sep = "")
+
+#Extract the results we want
+results.this.setting <- these.spearman.rank.correlation.between.GWAS.core.QTNs[which(names(these.spearman.rank.correlation.between.GWAS.core.QTNs)
+                                                                                     == this.setting)]
 
 
-#Load the R data object containing all results
-load("/Users/alipka/Library/CloudStorage/Box-Box/IR-281/analysis_results/analysis.1.FactorA..1.FactorB..0.05.FactorC..0.5.FactorD(7).Rdata")
+#Put the results into a format that we can extract the numbers from                                                                                                                                                                        == this.setting)]
+results.this.setting.for.figure <- matrix(unlist(results.this.setting), nrow = 3)
+
+#Some temporary code for testing out the nested for loop below
+#i <- 1
+#j <- 0.05
+#k <- 0.05
+#el <- 0.05
+#rep <- 1
+
+data.for.boxplot <- data.frame(spearman.rank.vector, factor.A.vector,
+                               factor.B.vector, factor.C.vector,
+                               factor.D.vector, rep.vector,
+                               pop.A.vector, pop.B.vector)
+View(data.for.boxplot)
+#End the temporary code for testing out the nested for loop below
+
+
+#End experimental code
+#######################################################
+#######################################################
+#######################################################
+#######################################################
 
 #######################################################
 ### Results for the core QTL
 #######################################################
+these.spearman.rank.correlation.between.GWAS.core.QTNs <- readRDS("/Users/alipka/Library/CloudStorage/Box-Box/IR-281/Master-Result-Lists-2024-06-13/master.these.spearman.rank.correlation.between.GWAS.core.QTNs.RDS")
+
+#Extract all of the results and put them into an object
+spearman.rank.vector <- NULL
+factor.A.vector <- NULL
+factor.B.vector <- NULL
+factor.C.vector <- NULL
+factor.D.vector <- NULL
+rep.vector <- NULL
+pop.A.vector <- NULL
+pop.B.vector <- NULL
+for(i in c(1,2,4)){
+  for(j in c(0.05,0.5,1,2)){
+    for(k in c(0.05,0.5,1,2)){
+      for(el in c(0.05,0.5,1,2)){
+        for(rep in 1:3){
+          #Get the object from the list you want
+          factor.A <- i
+          factor.B <- j
+          factor.C <- k
+          factor.D <- el 
+          this.rep <- rep
+          
+          #Read in the R data file for a given setting
+          this.setting <-  paste(factor.A,".FactorA..",factor.B, ".FactorB..",
+                                 factor.C,".FactorC..",factor.D,".FactorD..",
+                                 this.rep, ".Rep", sep = "")
+          
+          #Extract the results we want
+          results.this.setting <- these.spearman.rank.correlation.between.GWAS.core.QTNs[which(names(these.spearman.rank.correlation.between.GWAS.core.QTNs)
+                                                                                               == this.setting)]
+          
+          
+          #Put the results into a format that we can extract the numbers from                                                                                                                                                                        == this.setting)]
+          results.this.setting.for.figure <- matrix(unlist(results.this.setting), nrow = 3)
+          #Loop through the pairs of populations, and extract the information we need
+          for(this.row in 1:nrow(results.this.setting.for.figure)){
+            spearman.rank.vector <- c(spearman.rank.vector, results.this.setting.for.figure[this.row,3])
+            factor.A.vector <- c(factor.A.vector, factor.A)
+            factor.B.vector <- c(factor.B.vector, factor.B)
+            factor.C.vector <- c(factor.C.vector, factor.C)
+            factor.D.vector <- c(factor.D.vector, factor.D)
+            rep.vector <- c(rep.vector, this.rep)
+            pop.A.vector <-c(pop.A.vector, results.this.setting.for.figure[this.row,1])
+            pop.B.vector <- c(pop.B.vector, results.this.setting.for.figure[this.row,2])
+          }#End for(this.row in 1:nrow(results.this.setting.for.figure))
+        }#End for(rep in 1:3)
+      }#End for(el in 1:4)
+    }#End for(k in 1:4)
+  }#End for(j in 1:4)
+}#End for(i in 1:3)
+
+#Obtain the data for your boxplot
+data.for.boxplot <- data.frame(spearman.rank.vector, factor.A.vector,
+                               factor.B.vector, factor.C.vector,
+                               factor.D.vector, rep.vector,
+                               pop.A.vector, pop.B.vector)
+
 #Make a plot summarizing the GWAS results of core QTL
+#Ultimately, the information that I need for the box plots are
+# Value: in column 1 and Factor Level in column 2
+
 #Initiate the plot
- #For loop through the pairs of three subpopulations
+pdf("GWAS.Results.Core.QTL.written.while.writing.plot.code.pdf", width = 8)
+par(mfrow = c(3,4))
+ #Two for loops through the pairs of three subpopulations
+
   #For loop through the main effects of Factors A-D
     #For loop through the different levels of Factor i
         ####
         #Box plot of Spearman rank correlation coefficients of core QTL (Y-axis)
         ### Against factor levels (X-axis)
     #End for loop through the different levels of Factor i
-  #End for loop through the main effect pairs of populations
+
+  # End for loop through the main effect pairs of populations 
+   
 #End the plot
- 
+dev.off()
 
 #########
 #Make a plot of the variance component estimates of trait 2
