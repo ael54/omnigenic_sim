87f177ae146e0e7b067002538519c9c8dcace7a9
Alexander E. Lipka <ael54@cornell.edu>
2024-08-15 16:16:12 -0500

    Made it possible to compare GWAS results with previous generations

--- Master_Analysis_Script_20240807.R
+++ Master_Analysis_Script_20240807.R
@@ -11,33 +11,37 @@
 
 #Calculate the spearman correlation coefficients between additive 
 # effect estimates core and peripheral QTNs
-source("QG_Analysis_Code/Run_GWAS_on_QTNs_20240807.R")
+source("QG_Analysis_Code/Run_GWAS_on_QTNs_20240815.R")
 
 #Object for spearman correlation coefficients between core and peripheral QTNs
 spearman.correlations.between.core.QTNs
 median.spearman.correlations.between.core.QTN
 median.core.spearman.correlation.within.types.of.selection.QTN
+median.core.spearman.correlation.within.prev.gen.QTN
 median.core.spearman.correlation.different.types.of.selection.QTN
 
 spearman.correlations.between.peripheral.QTNs
 median.spearman.correlations.between.peripheral.QTN
 median.peripheral.spearman.correlation.within.types.of.selection.QTN
+median.peripheral.spearman.correlation.within.prev.gen.QTN 
 median.peripheral.spearman.correlation.different.types.of.selection.QTN
 
 #Calculate the spearman correlation coefficients between additive 
 # effect estimates of "Core SNPs" that are within 0.05 cM of core QTNs
 # and "Peripheral SNPs that are not"
-source("QG_Analysis_Code/Run_GWAS_on_SNPs_20240807.R")
+source("QG_Analysis_Code/Run_GWAS_on_SNPs_20240815.R")
 
 #Object for spearman correlation coefficients between core and peripheral QTNs
 spearman.correlations.between.core.SNPs
 median.spearman.correlations.between.core.SNPs
 median.core.spearman.correlation.within.types.of.selection.SNPs
+median.core.spearman.correlation.within.prev.gen.SNPs
 median.core.spearman.correlation.different.types.of.selection.SNPs
 
 spearman.correlations.between.peripheral.SNPs
-median.spearman.correlations.between.peripheral.SNPs
+median.spearman.correlations.between.peripheral.SNPs 
 median.peripheral.spearman.correlation.within.types.of.selection.SNPs
+median.peripheral.spearman.correlation.within.prev.gen.SNPs
 median.peripheral.spearman.correlation.different.types.of.selection.SNPs
  
 ################################################################################
@@ -60,11 +64,11 @@ median.peripheral.spearman.correlation.different.types.of.selection.SNPs
 ###Genomic prediction across subpopulations phase
 
 #For the QTLs
-source("QG_Analysis_Code/Run_GS_on_QTNs.R")
+source("QG_Analysis_Code/Run_GS_on_QTNs_20240814.R")
 
 
 
 #For the SNPs
-source("QG_Analysis_Code/Run_GS_on_SNPs.R")
+source("QG_Analysis_Code/Run_GS_on_SNPs_20240814.R")
 
 
