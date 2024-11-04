b08437a3ada3a4124072b258b200052a82df465e
Alexander E. Lipka <ael54@cornell.edu>
2024-11-04 17:06:51 -0600

    Made a whole bunch of new plots

---
+++ Functions_to_Make_Life_Easier/Old/Make_VC_Scatter_Plots_Breeding_Values_2024OLD.R
@@ -0,0 +1,48 @@
+#Make a box plot for Factor A  
+title.label = NA
+fa <- ggplot(input.scatter.plot.data, aes(x=generation.vector)) + 
+             geom_point(col ="purple", aes(y=log(breeding.value.1.variance))) + ylim(this.min.y.axis,this.max.y.axis) +
+  if(!is.na(title.label)) {
+    labs(title = title.label, x=paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), 
+         y = y.axis.label, size = 25)
+  }else{
+    labs(x= paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), y = y.axis.label, size = 25)
+  }
+
+title.label = NA
+fb <- ggplot(input.scatter.plot.data, aes(x=generation.vector)) + 
+  geom_point(col ="purple", aes(y=log(breeding.value.2.variance))) +  ylim(this.min.y.axis,this.max.y.axis) +
+  if(!is.na(title.label)) {
+    labs(title = title.label, x=paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), 
+         y = y.axis.label, size = 25)
+  }else{
+    labs(x= paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), y = y.axis.label, size = 25)
+  }
+
+
+
+title.label = NA
+fc <- ggplot(input.scatter.plot.data, aes(x=generation.vector)) + 
+  geom_point(col ="purple", aes(y=log(breeding.value.3.variance))) +  ylim(this.min.y.axis,this.max.y.axis) +
+  if(!is.na(title.label)) {
+    labs(title = title.label, x=paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), 
+         y = y.axis.label, size = 25)
+  }else{
+    labs(x= paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), y = y.axis.label, size = 25)
+  }
+
+
+title.label = NA
+fd <- ggplot(input.scatter.plot.data, aes(x=generation.vector)) + 
+  geom_point(col ="purple", aes(y=log(breeding.value.4.variance)))+  ylim(this.min.y.axis,this.max.y.axis) +
+  if(!is.na(title.label)) {
+    labs(title = title.label, x=paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), 
+         y = y.axis.label, size = 25)
+  }else{
+    labs(x= paste("Factor = ", substr(colnames(data.for.vc.scatterplot)[this.factor],start = 7,stop = 9),", Level = ",  this.level, ", Selection = ", this.selection.type, ", No. Inds. Selected = ", this.selection.number, sep = ""), y = y.axis.label, size = 25)
+  }
+
+
+
+ 
+  
